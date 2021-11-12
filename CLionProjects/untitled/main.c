0bd5858f50bab3a757490eac3102334f01b55829
tafman2002 <72532930+tafman2002@users.noreply.github.com>
2021-11-12 15:56:52 -0600

    .git

---
+++ CLionProjects/untitled/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    printf("Welcome back!\n");
+    double f = 0;
+    scanf("%lf", &f);
+    printf("F is %.2f", f);
+    return 0;
+}
